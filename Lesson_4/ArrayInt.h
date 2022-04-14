f35e2c1cff0c1f35780ebdfddbac6b1e2f6ad909
Arthur <83765685+atarovart@users.noreply.github.com>
2022-04-14 18:36:36 +0700

    Add files via upload

---
+++ Lesson_4/ArrayInt.h
@@ -0,0 +1,183 @@
+﻿#pragma once
+#ifndef ARRAYINT_H
+#define ARRAYINT_H
+
+#include <cassert>
+#include <iostream>
+
+class ArrayInt
+{
+private:
+    int m_length;
+    int* m_data;
+
+public:
+    ArrayInt() 
+        : m_length(0), m_data(nullptr)
+    {
+
+    }
+
+    ArrayInt(int length) 
+        : m_length(length)
+    {
+        assert(length >= 0);
+
+        if (length > 0)
+            m_data = new int[length];
+        else
+            m_data = nullptr;
+    }
+
+    void erase()
+    {
+        delete[] m_data;
+
+        m_data = nullptr;
+        m_length = 0;
+    }
+
+    int getLength() 
+    { 
+        return m_length;
+    }
+
+    int& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
+    }
+
+
+    void resize(int newLength)
+    {
+
+        if (newLength == m_length)
+            return;
+
+
+        if (newLength <= 0)
+        {
+            erase();
+            return;
+        }
+
+        int* data = new int[newLength];
+
+   
+        if (m_length > 0)
+        {
+            int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+
+            for (int index = 0; index < elementsToCopy; ++index)
+                data[index] = m_data[index];
+        }
+
+        delete[] m_data;
+
+        m_data = data;
+        m_length = newLength;
+    }
+
+    void insertBefore(int value, int index)
+    {
+
+        assert(index >= 0 && index <= m_length);
+
+  
+        int* data = new int[m_length + 1];
+
+ 
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+
+   
+        data[index] = value;
+
+        for (int after = index; after < m_length; ++after)
+            data[after + 1] = m_data[after];
+
+        delete[] m_data;
+        m_data = data;
+        ++m_length;
+    }
+
+    //Task 1 
+    void deleteIndex(int index)
+    {
+        assert(index >= 0 && index <= m_length);
+
+        int* newData = new int[m_length - 1];
+        int j = 0;
+
+        for (int i = 0; i < m_length; i++)
+        {
+            if (i == index)
+            {
+            }
+            else
+            {
+                newData[j] = m_data[i];
+                j++;
+            }
+            
+        }
+
+        delete[] m_data;
+        m_data = newData;
+        --m_length;
+
+    }
+
+    void pop_back()
+    {
+        deleteIndex(m_length - 1);
+    }
+
+    void pop_front()
+    {
+        deleteIndex(0);
+    }
+
+    void sort()
+    {
+        int temp;
+        for (int i = 0; i < m_length; i++)
+        {
+            for (int j = 0; j < m_length - 1; j++)
+            {
+                if (m_data[j] > m_data[j + 1])
+                {
+                    temp = m_data[j + 1];
+                    m_data[j + 1] = m_data[j];
+                    m_data[j] = temp;
+                }
+            }
+        }
+    }
+
+    void print()
+    {
+        std::cout << "\n";
+        for (int i = 0; i < m_length; i++)
+        {
+            std::cout << m_data[i] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    //Task 1 part2
+
+    void push_back(int value) 
+    { 
+        insertBefore(value, m_length); 
+    }
+
+    ~ArrayInt()
+    {
+        delete[] m_data;
+    }
+};
+
+#endif
+
---
+++ Lesson_4/Lesson_4.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "ArrayInt.h"
+#include "BlackJack.h"
+
+
+//Task 1
+void Task1()
+{
+    ArrayInt array;
+    array.push_back(5);
+    array.push_back(3);
+    array.push_back(2);
+    array.push_back(16);
+    array.push_back(8);
+    array.push_back(7);
+    array.push_back(9);
+    array.push_back(9);
+    array.push_back(8);
+    array.push_back(8);
+
+
+    std::cout << "Initial array:";
+    array.print();
+
+    array.pop_back();
+    std::cout << "Array after pop_back:";
+    array.print();
+
+    array.pop_front();
+    std::cout << "Array after pop_front:";
+    array.print();
+
+    array.sort();
+    std::cout << "Sorted array:";
+    array.print();
+}
+
+//Task 2
+void printVector(std::vector<int>& input)
+{
+    std::cout << "\n";
+    for (std::vector<int>::iterator it = input.begin(); it != input.end(); ++it)
+        std::cout << *it << " ";
+}
+int countUniqueNumbers(std::vector<int> input)
+{
+    std::sort(input.begin(), input.end());
+    std::vector<int>::iterator it;
+    it = std::unique(input.begin(), input.end());
+    input.resize(std::distance(input.begin(), it));
+    //printVector(input);
+    return input.size();
+}
+
+
+int main()
+{
+    Task1();
+
+    //Task 2
+
+    std::vector<int> array = { 2, 3, 5, 6, 6, 6, 7, 6, 1, 1, 2, 11, 24, 5311 };
+    std::cout << "\nInitial array:";
+    printVector(array);
+    int count = countUniqueNumbers(array);
+    std::cout << "\nUnique numbers: " << count;
+
+    //Task 3
+
+    Card aceClubs(ACE, CLUBS);
+    Card aceHearts(ACE, HEARTS);
+    Card kingClubs(KING, CLUBS);
+
+    Hand playerHand;
+    playerHand.Add(&aceClubs);
+    playerHand.Add(&aceHearts);
+    playerHand.Add(&kingClubs);
+
+    int sum = playerHand.GetValue();
+    std::cout << "\n\nPlayer's hand value is: " << sum;
+    playerHand.Clear();
+    sum = playerHand.GetValue();
+    std::cout << "\nHand cleared! New value is: " << sum;
+}
+
