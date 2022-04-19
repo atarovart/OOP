b1e389796d9b4d24619f697ea7f6b42b6e736da5
Arthur <83765685+atarovart@users.noreply.github.com>
2022-04-19 22:05:41 +0700

    Add files via upload

---
+++ Lesson_6/Lesson_6.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+
+//Task 1 
+
+int getInt()
+{
+    std::string input;
+    int intInput = 0;
+
+    while (true)
+    {
+        bool formatCheck = true;
+        std::cout << "Enter your number: ";
+        std::getline(std::cin, input);
+        for (unsigned int i = 0; i < input.length(); i++)
+        {
+            if (!isdigit(input[i])) formatCheck = false;
+        }
+        if (formatCheck)
+        {
+            intInput = std::stoi(input);
+            return intInput;
+        }
+        else
+        {
+            system("cls");
+            std::cerr << "Incorrect format!" << std::endl;
+            std::cin.clear();
+            //std::cin.ignore(10000, '\n');           
+        }
+    }
+}
+
+//Task 2 
+
+std::ostream& endll(std::ostream& stream)
+{
+    stream << "\n\n";
+    stream.clear();
+    return stream;
+}
+
+int main()
+{
+    int result = getInt();
+    std::cout << "Your number is " << result << endll << "Task 2 test";
+}
